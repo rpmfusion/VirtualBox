d1f23de7685109a696f41512e947678514dcfcc5
Sérgio M. Basto <sergio@serjux.com>
2017-10-31 02:00:12 +0000

    fix typo in update script

--- update_vbox.sh
+++ update_vbox.sh
@@ -7,7 +7,7 @@ rfpkg clog && rfpkg commit -F clog && /bin/rm clog && git show
 echo Press enter to continue; read dummy;
 rfpkg push && rfpkg build --nowait
 echo Press enter to continue; read dummy;
-git checkout f24 && git merge master && git push && rfpkg build --nowait; git checkout master
+git checkout f27 && git merge master && git push && rfpkg build --nowait; git checkout master
 echo Press enter to continue; read dummy;
 git checkout f26 && git merge master && git push && rfpkg build --nowait; git checkout master
 echo Press enter to continue; read dummy;
