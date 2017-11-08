e6c2bce4cda6123bf88f4e58a5e7f2f05dfcc855
Sérgio M. Basto <sergio@serjux.com>
2017-11-08 17:26:45 +0000

    update automate script

--- update_vbox.sh
+++ update_vbox.sh
@@ -15,15 +15,6 @@ git checkout f25 && git merge master && git push && rfpkg build --nowait; git ch
 echo Press enter to continue; read dummy;
 git checkout el7 && git merge master && git push && rfpkg build --nowait; git checkout master
 
-koji-rpmfusion tag-build f27-free-override VirtualBox-$VERSION-1.fc27
-koji-rpmfusion wait-repo f27-free-build --build=VirtualBox-$VERSION-1.fc27 &
-koji-rpmfusion tag-build f26-free-override VirtualBox-$VERSION-1.fc26
-koji-rpmfusion wait-repo f26-free-build --build=VirtualBox-$VERSION-1.fc26 &
-koji-rpmfusion tag-build f25-free-override VirtualBox-$VERSION-1.fc25
-koji-rpmfusion wait-repo f25-free-build --build=VirtualBox-$VERSION-1.fc25 &
-koji-rpmfusion tag-build el7-free-override VirtualBox-$VERSION-1.el7
-koji-rpmfusion wait-repo el7-free-build --build=VirtualBox-$VERSION-1.el7 &
-
 cd ../VirtualBox-kmod/
 git pull
 rpmdev-bumpspec -n $VERSION -c "Update VBox to $VERSION" VirtualBox-kmod.spec
@@ -33,11 +24,20 @@ rfpkg clog && rfpkg commit -F clog && /bin/rm clog && git show
 #git rm kernel-4.10.0-0.rc5.lnkops.v2.patch
 echo Press enter to continue; read dummy;
 rfpkg push && rfpkg build --nowait
+
 echo Press enter to continue; read dummy;
+koji-rpmfusion tag-build f27-free-override VirtualBox-$VERSION-1.fc27
+koji-rpmfusion wait-repo f27-free-build --build=VirtualBox-$VERSION-1.fc27
 git checkout f27 && git merge master && git push && rfpkg build --nowait; git checkout master
 echo Press enter to continue; read dummy;
+koji-rpmfusion tag-build f26-free-override VirtualBox-$VERSION-1.fc26
+koji-rpmfusion wait-repo f26-free-build --build=VirtualBox-$VERSION-1.fc26
 git checkout f26 && git merge master && git push && rfpkg build --nowait; git checkout master
 echo Press enter to continue; read dummy;
+koji-rpmfusion tag-build f25-free-override VirtualBox-$VERSION-1.fc25
+koji-rpmfusion wait-repo f25-free-build --build=VirtualBox-$VERSION-1.fc25
 git checkout f25 && git merge master && git push && rfpkg build --nowait; git checkout master
 echo Press enter to continue; read dummy;
+koji-rpmfusion tag-build el7-free-override VirtualBox-$VERSION-1.el7
+koji-rpmfusion wait-repo el7-free-build --build=VirtualBox-$VERSION-1.el7
 git checkout el7 && git merge master && git push && rfpkg build --nowait; git checkout master
