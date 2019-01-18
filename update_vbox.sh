a2704261390e6ea92b7d9c285bb08b7ef1cd08b7
Vasiliy Glazov <v.glazov@javad.com>
2019-01-18 09:28:38 +0300

    Revert "more updates on script helper"
    This reverts commit bc5b54db4d594748693b039920fce9b6eed19313.

--- update_vbox.sh
+++ update_vbox.sh
@@ -47,4 +47,30 @@ echo STAGE 5
 git pull
 rpmdev-bumpspec -n $VERSION -c "Update VBox to $VERSION" VirtualBox-kmod.spec
 rfpkg srpm && copr-cli build sergiomb/vboxfor23 VirtualBox-kmod-$VERSION-$REL.fc$RAWHIDE.src.rpm
-echo Continue in ..../VirtualBox-kmod/update_vbox.sh
+rfpkg ci -c && git show
+#cp VirtualBox-kmod.spec VirtualBox-kmod.spec.new
+#git reset HEAD~1
+#git rm kernel-4.10.0-0.rc5.lnkops.v2.patch
+echo Press enter to continue; read dummy;
+rfpkg push && rfpkg build --nowait
+
+echo Press enter to continue; read dummy;
+#koji-rpmfusion watch-task
+koji-rpmfusion tag-build f27-free-override VirtualBox-$VERSION-$REL.fc27
+koji-rpmfusion wait-repo f27-free-build --build=VirtualBox-$VERSION-$REL.fc27
+git checkout f27 && git merge master && git push && rfpkg build --nowait; git checkout master
+echo Press enter to continue; read dummy;
+koji-rpmfusion tag-build f26-free-override VirtualBox-$VERSION-$REL.fc26
+
+echo "koji-rpmfusion tag-build f26-free-override VirtualBox-$VERSION-$REL.fc26
+koji-rpmfusion wait-repo f26-free-build --build=VirtualBox-$VERSION-$REL.fc26
+git checkout f26 && git merge master && git push && rfpkg build --nowait; git checkout master
+Press enter to continue; read dummy;
+koji-rpmfusion tag-build f25-free-override VirtualBox-$VERSION-$REL.fc25
+koji-rpmfusion wait-repo f25-free-build --build=VirtualBox-$VERSION-$REL.fc25
+git checkout f25 && git merge master && git push && rfpkg build --nowait; git checkout master
+Press enter to continue; read dummy;
+koji-rpmfusion tag-build el7-free-override VirtualBox-$VERSION-$REL.el7
+koji-rpmfusion wait-repo el7-free-build --build=VirtualBox-$VERSION-$REL.el7
+git checkout el7 && git merge master && git push && rfpkg build --nowait; git checkout master"
+fi
